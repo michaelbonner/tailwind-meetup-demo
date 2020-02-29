92faa11f8b37243d80d9b9c227ad90ec528194ce
Michael Bonner <michaelwbonner@gmail.com>
2020-02-29 16:22:08 -0700

    simple components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PricingTall from "./components/PricingTall";
+import PricingWide from "./components/PricingWide";
 
-function App() {
+const links = [
+  {
+    title: "Tailwind CSS Meetup Demo",
+    path: "/"
+  },
+  {
+    title: "Pricing Wide",
+    path: "/pricing-wide"
+  },
+  {
+    title: "Pricing Tall",
+    path: "/pricing-tall"
+  }
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="antialiased font-sans">
+        <nav className="bg-indigo-400 text-white text-xl font-semibold">
+          <ul className="flex py-4">
+            {links.map(link => (
+              <li className="px-4" key={link.path}>
+                <Link to={link.path}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/pricing-tall">
+            <PricingTall />
+          </Route>
+          <Route path="/pricing-wide">
+            <PricingWide />
+          </Route>
+          <Route path="/">
+            <div className="w-full py-24 flex flex-col justify-center items-center">
+              <div className="text-5xl">Hello Meetup</div>
+              <a className="w-64 mt-14" href="https://tailwindcss.com/">
+                <img
+                  alt="Tailwind CSS"
+                  className="w-64"
+                  src={require("./images/tailwind-logo.svg")}
+                />
+              </a>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
